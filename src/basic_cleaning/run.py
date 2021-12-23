f123899219f0eb0f55238b2ef93dbf45b42b9b1e
leohsuofnthu <leohsuofnthu@gmail.com>
2021-12-22 23:07:41 -0500

    add extra cleaning for longitude and latitude

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -32,6 +32,10 @@ def go(args):
     logger.info("Covert 'last_review' to datetime object")
     df["last_review"] = pd.to_datetime(df["last_review"])
 
+    # limit the range of longitude & latitude
+    idx = df["longitude"].between(-74.25, -73.50) & df["latitude"].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # Saving and log the artifact
     filename = args.output_artifact
     df.to_csv(filename, index=False)
